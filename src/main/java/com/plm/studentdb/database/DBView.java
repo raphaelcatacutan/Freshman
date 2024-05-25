bb563e0e4e135e4c6b46ac105c65485cbc035a9f
Trisha <tmrlau2023@plm.edu.ph>
2024-05-26 00:34:45 +0800

    Back-end Operations View Added!!!

--- src/main/java/com/plm/studentdb/database/DBView.java
+++ src/main/java/com/plm/studentdb/database/DBView.java
@@ -1,5 +1,25 @@
 package com.plm.studentdb.database;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 public class DBView {
-
+    public static void viewStudentRecord(){
+        String selectQuery = "SELECT * FROM student_record";
+        try (PreparedStatement p = DBConnection.getConnection().prepareStatement(selectQuery)){
+            p.getConnection().prepareStatement(selectQuery);
+            ResultSet rs=p.executeQuery();
+            while(rs.next()){
+                int ID = rs.getInt("student_id");
+                String name = rs.getString("name");
+                String crs = rs.getString("course");
+                int yr = rs.getInt("year");
+                double finGrade = rs.getDouble("final_grade");
+                double gwa = rs.getDouble("gwa");
+                String stats = rs.getString("status");
+                System.out.println(ID + "\t\t" + name + "\t\t\t" + crs + "\t\t\t" + yr + "\t\t" + finGrade + "\t\t" + gwa + "\t\t" + stats);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error fetching student record: " + e.getMessage());
+        }
+    }
 }
