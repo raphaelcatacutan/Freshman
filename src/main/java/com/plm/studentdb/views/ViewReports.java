f4bce137b6af483d0287cf4e7f2e44a3e58e362b
kkatoki <raphaelcatacutan@gmail.com>
2024-06-05 20:13:58 +0800

    Removed Errors on ViewReports.java

--- src/main/java/com/plm/studentdb/views/ViewReports.java
+++ src/main/java/com/plm/studentdb/views/ViewReports.java
@@ -1,18 +1,18 @@
 package com.plm.studentdb.views;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.MenuButton;
 
-public class ViewReports implements Initializable {
+public class ViewReports  {
 
-    @FXML private MenuButton<String> Menu1;
-    @FXML private MenuButton<String> Menu2;
-    @FXML private MenuButton<String> Menu3;
-    @FXML private MenuButton<String> Menu4;
+    @FXML private MenuButton Menu1;
+    @FXML private MenuButton Menu2;
+    @FXML private MenuButton Menu3;
+    @FXML private MenuButton Menu4;
 
-    @Override
-    public void initizalize ();
+    public void initizalize() {
+
+    }
 }
 
 
