57f2cd103d658f84384f422a8e7f5e5050f02806
Charliz Dana <charlizdana@gmail.com>
2024-06-05 14:38:23 +0800

    View reports UI

--- src/main/java/com/plm/studentdb/views/ViewReports.java
+++ src/main/java/com/plm/studentdb/views/ViewReports.java
@@ -1,4 +1,18 @@
 package com.plm.studentdb.views;
 
-public class ViewReports {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuButton;
+
+public class ViewReports implements Initializable {
+
+    @FXML private MenuButton<String> Menu1;
+    @FXML private MenuButton<String> Menu2;
+    @FXML private MenuButton<String> Menu3;
+    @FXML private MenuButton<String> Menu4;
+
+    @Override
+    public void initizalize ();
 }
+
+
