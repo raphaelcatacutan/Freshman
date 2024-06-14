441f73c8dc029518af1bdd4d5269d2b3f7285252
kkatoki <raphaelcatacutan@gmail.com>
2024-06-14 19:53:36 +0800

    Added ProgramConstants.java

--- src/main/java/com/plm/studentdb/utils/ProgramConstants.java
+++ src/main/java/com/plm/studentdb/utils/ProgramConstants.java
@@ -1,4 +1,10 @@
 package com.plm.studentdb.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgramConstants {
+    public static String accountName;
+    public static List<String> accountAccess = new ArrayList<>();
+    public static int accountID;
 }
